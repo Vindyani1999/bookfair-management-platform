54d1d6c0d393e8578b75911095706046548cd011
vidusha <dilshanariyarathna1999@gmail.com>
2025-11-04 16:50:44 +0530

    basic setup

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -5,13 +5,17 @@ import FAQ from "./pages/info/FAQPage";
 import TermsOfService from "./pages/info/TermsOfServicePage";
 import PrivacyPolicy from "./pages/info/PrivacyPolicyPage";
 import TemporaryStepperPage from "./pages/TemporaryStepperPage";
+import DrawerLayout from "./components/Layout/DrawerLayout";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/book" element={<TemporaryStepperPage />} />
+        <Route path="/" element={<DrawerLayout/>}>
+          <Route path="book" element={<TemporaryStepperPage />} />
+        </Route>
+        
         <Route path="/about" element={<About />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/terms" element={<TermsOfService />} />
