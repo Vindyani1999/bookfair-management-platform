0cfa3f9f5c550dfc26390380ed52ec3b993d86d8
nushan hansana <eanushanhansana@gmail.com>
2025-11-04 17:28:37 +0530

    halls are added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { connectDB, sequelize } = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const hallRoutes = require('./routes/hallRoutes');
+const stallRoutes = require('./routes/stallRoutes');
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.get('/', (req, res) => res.send('SIB-RMS Backend is running'));
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/hall', hallRoutes);
-
+app.use('/api/v1/stall', stallRoutes);
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
