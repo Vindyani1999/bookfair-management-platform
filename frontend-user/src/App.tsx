8b103105fd114519d1d2dd990bd84f2af2384854
Hashini <prabuddhikarph@gmail.com>
2025-11-06 19:12:27 +0530

    developed signup page

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -1,5 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from './context/AuthContext';
 import LandingPage from "./pages/LandingPage";
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import UserDashboard from './pages/UserDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 import About from "./pages/info/AboutPage";
 import FAQ from "./pages/info/FAQPage";
 import TermsOfService from "./pages/info/TermsOfServicePage";
@@ -10,17 +15,30 @@ import DrawerLayout from "./components/Layout/DrawerLayout";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/drawer/" element={<DrawerLayout/>}>
-          {/* <Route path="book" element={<TemporaryStepperPage />} /> */}
-        </Route>
-        <Route path="/book" element={<TemporaryStepperPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/terms" element={<TermsOfService />} />
-        <Route path="/privacy" element={<PrivacyPolicy />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/drawer/" element={<DrawerLayout />}>
+            {/* <Route path="book" element={<TemporaryStepperPage />} /> */}
+          </Route>
+          <Route path="/book" element={<TemporaryStepperPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <UserDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
