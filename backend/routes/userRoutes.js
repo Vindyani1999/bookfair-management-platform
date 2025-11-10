d45f8c670ad0f7205501b3477018dc5ba1e55630
chamindu77 <chamindunipun99@gmail.com>
2025-11-10 08:01:45 +0530

    feat: Implement authentication and role-based authorization for user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers, getUserById, updateUser, deleteUser } = require('../controllers/userController');
 
-router.get('/', getAllUsers);         
-router.get('/:id', getUserById);      
-router.put('/:id', updateUser);       
-router.delete('/:id', deleteUser);    
+const {
+  authenticate,
+  authorizeRoles,
+} = require('../middleware/authMiddleware');
+
+
+router.get('/', authenticate, authorizeRoles('admin'), getAllUsers);         
+router.get('/:id', authenticate, authorizeRoles('admin, user'), getUserById);      
+router.put('/:id', authenticate, authorizeRoles('admin'), updateUser);       
+router.delete('/:id', authenticate, authorizeRoles('admin'), deleteUser);    
 
 module.exports = router;
