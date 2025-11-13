b6ff71b6338347b060ab25b786661a6dab246dd9
chamindu77 <chamindunipun99@gmail.com>
2025-11-13 08:33:05 +0530

    test: no issues admin login

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,50 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE,
-  process.env.MYSQL_USER,
-  process.env.MYSQL_PASSWORD,
-  {
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
-    logging: false
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    connectTimeout: 60000, 
+  },
+});
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log('MySQL connected successfully using Sequelize');
-  } catch (err) {
-    console.error('Unable to connect to MySQL:', err);
+    console.log('MySQL connected successfully to Railway using Sequelize');
+  } catch (error) {
+    console.error('Unable to connect to MySQL:', error.message);
     process.exit(1);
   }
 };
 
 module.exports = { sequelize, connectDB };
+
+
+
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQL_DATABASE,
+//   process.env.MYSQL_USER,
+//   process.env.MYSQL_PASSWORD,
+//   {
+//     host: process.env.MYSQL_HOST,
+//     dialect: 'mysql',
+//     logging: false
+//   }
+// );
+
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('MySQL connected successfully using Sequelize');
+//   } catch (err) {
+//     console.error('Unable to connect to MySQL:', err);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = { sequelize, connectDB };
