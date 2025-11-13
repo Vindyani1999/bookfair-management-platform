894813b0c9a0c081e047d71e2fe21b0d5a505ef4
chamindu77 <chamindunipun99@gmail.com>
2025-11-13 16:54:42 +0530

    Refactor database connection code to remove commented-out legacy configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,3 @@
-
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -22,31 +21,4 @@ const connectDB = async () => {
 };
 
 module.exports = { sequelize, connectDB };
-
-
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.MYSQL_DATABASE,
-//   process.env.MYSQL_USER,
-//   process.env.MYSQL_PASSWORD,
-//   {
-//     host: process.env.MYSQL_HOST,
-//     dialect: 'mysql',
-//     logging: false
-//   }
-// );
-
-// const connectDB = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('MySQL connected successfully using Sequelize');
-//   } catch (err) {
-//     console.error('Unable to connect to MySQL:', err);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = { sequelize, connectDB };
 
