913021b651417a9942236be1dcb008a66832bffa
Ketawala_MH <ketawala_mh_e22@engug.ruh.ac.lk>
2025-11-24 15:57:54 +0530

    Change db data type in transaction tabl

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -32,7 +32,7 @@ const Transaction = sequelize.define('Transaction', {
         allowNull: false
     },
     sessionUrl: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
     },
     isPaid: {
